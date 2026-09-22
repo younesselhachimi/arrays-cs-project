4f56c8d2c3ed174fe69215f95f6295feb5861587
Youness EL HACHIMI <younesselhachimi3@gmail.com>
2026-09-22 15:07:20 +0000

    the CourseNumbersArray code

--- src/main/java/samplearrays/CourseNumbersArray.java
+++ src/main/java/samplearrays/CourseNumbersArray.java
@@ -3,6 +3,19 @@ package samplearrays;
 public class CourseNumbersArray {
     public static void main(String[] args) {
         int[] registeredCourses = {1010, 1020, 2080, 2140, 2150, 2160};
+        int newCourse = 2222;
+        int[] updatedCourses = new int[registeredCourses.length+1];
+        for(int i=0;i<registeredCourses.length;i++){
+            updatedCourses[i]=registeredCourses[i];
+        }
+        updatedCourses[updatedCourses.length-1]=newCourse;
+
+        for(int i=0;i<updatedCourses.length-1;i++){
+            System.out.print(updatedCourses[i]);
+            System.out.print(" | ");
+        }
+        System.out.println(updatedCourses[updatedCourses.length-1]);
+
 
     }
 }
