2d1122d5e6e88736237dbcb5339e40ebb6b0bb4b
Youness EL HACHIMI <younesselhachimi3@gmail.com>
2026-09-22 15:59:14 +0000

    my code of DogShelter code

--- src/main/java/samplearrays/DogShelter.java
+++ src/main/java/samplearrays/DogShelter.java
@@ -3,7 +3,7 @@ package samplearrays;
 public class DogShelter {
 
     // initialize an array of 3
-
+    private static Integer[] dogCounts= new Integer[3];
 
     public static void main(String[] args) {
 
@@ -30,16 +30,21 @@ public class DogShelter {
     // Add count to a given index
     public static void addBreed(int index, int count) {
         // add you code here
+        dogCounts[index]=dogCounts[index]+count;
     }
 
     // Remove a breed by setting its count to 0
     public static void deleteBreed(int index) {
         // add your code here
+        dogCounts[index]=0;
     }
 
     // Display all dog counts
     public static void displayDogs() {
         // add your code
+        for(int i=0;i<dogCounts.length;i++){
+            System.out.println("Breed " + i + " has " + dogCounts[i] + " dogs.");
+        }
           //  System.out.println("Breed " + i + " has " + dogCounts[i] + " dogs.");
 
     }
