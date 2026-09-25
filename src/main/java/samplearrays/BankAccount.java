671b254e8a095386d829949b36e629c77225e5ad
Youness EL HACHIMI <younesselhachimi3@gmail.com>
2026-09-25 12:04:18 +0000

    BankAccount class methods

--- src/main/java/samplearrays/BankAccount.java
+++ src/main/java/samplearrays/BankAccount.java
@@ -5,25 +5,57 @@ public class BankAccount {
     String name;
     double currentBalance;
     //TO-DO: Initialize an Array with 1000 in size that stores Double called 'transactions' to keep track of the user's transactions
+    double[] transactions = new double[1000];
+    int transaction_Count = 0;
 
     public BankAccount(String name, int startingBalance){
-
+        this.name=name;
+        this.currentBalance = startingBalance;
     }
 
     public void deposit(double amount){
-
+        if(amount<=0){
+            System.out.println("The amount must be positive.");
+            return ;
+        }
+        currentBalance+=amount;
+        transactions[transaction_Count] =amount;
+        transaction_Count++;
+        System.out.println(name + " diposit : " + amount + ". New balance : " + currentBalance);
     }
 
     public void withdraw(double amount){
+        if(amount<=0){
+            System.out.println("The amount must be positive.");
+            return ;
+        }
+        if(amount>currentBalance){
+            System.out.println("unsuccessful withdraw. The amount is insufficient");
+            return ;
+        }
+        currentBalance-=amount;
+        transactions[transaction_Count]= (-amount);
+        transaction_Count++;
+        System.out.println("New balance after withdrawis  : " + currentBalance);
 
     }
 
     public void displayTransactions(){
+        if (transaction_Count == 0) {
+            System.out.println("No transactions .");
+            return;
+        }
 
+        System.out.println("Your list of transactions is the following: ");
+        for(int i=0; i<transaction_Count-1; i++) {
+            System.out.print(transactions[i]);
+            System.out.print(" | ");
+        }
+        System.out.println(transactions[transaction_Count-1]);
     }
 
     public void displayBalance(){
-
+        System.out.println("The current balance of the account is : " + currentBalance);
     }
 
     public static void main(String[] args) {
